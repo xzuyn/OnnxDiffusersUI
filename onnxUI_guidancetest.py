31de1ff51494e0f8e72e2b8dee20ddfcfb166e7b
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-02-20 23:02:48 -0500

    Update onnxUI_guidancetest.py

--- onnxUI_guidancetest.py
+++ onnxUI_guidancetest.py
@@ -638,7 +638,7 @@ def run_diffusers(
     short_prompt = prompt.strip('<>:"/\\|?*\n\t')
     short_prompt = re.sub(r'[\\/*?:"<>|\n\t]', "", short_prompt)
     short_prompt = (
-        short_prompt[:64] if len(short_prompt) > 32 else short_prompt
+        short_prompt[:50] if len(short_prompt) > 32 else short_prompt
     )
     frames_path = (
             output_path + f"/videoframes/"
